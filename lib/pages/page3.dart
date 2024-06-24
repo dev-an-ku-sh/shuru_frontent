04f31f92cfd7ccf7de16871702105e848d6b561c
dev-an-ku-sh <133857844+dev-an-ku-sh@users.noreply.github.com>
2024-06-24 14:00:49 +0530

    added ai cards

--- lib/pages/page3.dart
+++ lib/pages/page3.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shuru_frontent/backend/state.dart';
+import 'package:shuru_frontent/pages/page4.dart';
 
 class Page3 extends ConsumerStatefulWidget {
   const Page3({super.key});
@@ -23,21 +24,46 @@ class _Page3State extends ConsumerState<Page3> {
     PersonaList = ref.watch(personaListProvider);
     return Scaffold(
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              height: 300,
-              width: 240,
-              color: Colors.red,
-              child: Text(PersonaList.toString()),
-            ),
-          ],
-        ),
+        child: PersonaList.isEmpty
+            ? CircularProgressIndicator() // Show loading indicator when list is empty
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: List.generate(PersonaList.length, (index) {
+                  if (PersonaList.length > index &&
+                      PersonaList[index].length >= 2) {
+                    return Card(
+                      color: Colors.blueGrey,
+                      child: Container(
+                        height: 300,
+                        width: 240,
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(PersonaList[index][0],
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.white)),
+                            SizedBox(height: 10), // Spacing between texts
+                            Text(PersonaList[index][1],
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.white)),
+                          ],
+                        ),
+                      ),
+                    );
+                  } else {
+                    // Return an empty container if the condition is not met
+                    return Container();
+                  }
+                }),
+              ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Navigator.pop(context);
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const Page4()),
+          );
         },
         child: const Icon(Icons.arrow_forward),
       ),
