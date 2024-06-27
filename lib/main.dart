cf2dd544ac85edd8091b00743ca4ad4a4a94988e
dev-an-ku-sh <133857844+dev-an-ku-sh@users.noreply.github.com>
2024-06-27 09:44:08 +0530

    optimised assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shuru_frontent/pages/page1.dart';
-import 'package:shuru_frontent/pages/page3.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyMaterialBase()));
